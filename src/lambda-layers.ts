6fa01eb0586225a298f7441f340a0d9f8d542251
Dan Mills <dwm@bluetel.co.uk>
2023-10-06 15:10:16 +0100

    Use axios

--- src/lambda-layers.ts
+++ src/lambda-layers.ts
@@ -1,53 +1,20 @@
-import https from 'https'
-import http from 'http'
+import axios, { AxiosRequestConfig } from 'axios'
 
 export const request = async (
   url: string,
-  options?: https.RequestOptions,
+  headers?: { [key: string]: string },
   method?: 'GET' | 'POST' | 'PUT' | 'DELETE',
   body?: any
 ): Promise<{ [key: string]: any }> => {
-  const formattedUrl = new URL(url)
-
-  const moduleToUse = formattedUrl.protocol === 'http:' ? http : https
-
-  const requestOptions: https.RequestOptions = {
-    hostname: formattedUrl.hostname,
-    port: formattedUrl.port,
-    path: formattedUrl.pathname,
-    ...options,
+  const config: AxiosRequestConfig = {
+    method: method ?? 'POST',
+    url,
     headers: {
       'Content-Type': 'application/json',
-      ...options?.headers,
+      ...(headers ?? {}),
     },
+    data: method === 'POST' && body ? body : undefined,
   }
-
-  return new Promise((resolve, reject) => {
-    const request = moduleToUse.request(requestOptions, (response) => {
-      let returnData = ''
-
-      response.on('data', (chunk) => {
-        returnData += chunk
-      })
-
-      response.on('end', () => {
-        try {
-          const parsedJson = JSON.parse(returnData)
-          resolve(parsedJson)
-        } catch (err) {
-          reject(err)
-        }
-      })
-    })
-
-    request.on('error', (err) => {
-      reject(err)
-    })
-
-    if (body && method === 'POST') {
-      request.write(JSON.stringify(body))
-    }
-
-    request.end()
-  })
+  const response = await axios(config)
+  return response.data
 }
